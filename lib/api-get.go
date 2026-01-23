ea95d111d7fd2c7c6db1d76472e446053356a0b1
wangwenru <13640661@qq.com>
2026-01-24 02:41:38 +0800

    init

--- lib/api-get.go
+++ lib/api-get.go
@@ -16,6 +16,10 @@ func MainSystem(userToken string, apiPath string, data interface{}, result inter
 	apiUrl, _ := beego.AppConfig.String("Sanzhizhou.ApiUrl")
 	return ApiGet(userToken, apiUrl, apiPath, data, result)
 }
+func MainSystemPost(userToken string, apiPath string, data interface{}, result interface{}) ([]byte, error) {
+	apiUrl, _ := beego.AppConfig.String("Sanzhizhou.ApiUrl")
+	return ApiPost(userToken, apiUrl, apiPath, data, result)
+}
 
 func SubsystemVideoPublish(userId string, apiPath string, data interface{}, result interface{}) ([]byte, error) {
 	apiUrl, _ := beego.AppConfig.String("Subsystem.VideoPublishAPIUrl")
@@ -66,6 +70,59 @@ func ApiGet(userToken string, apiUrl, apiPath string, data interface{}, result i
 	if errJson != nil {
 		fmt.Println("err 1:", url)
 		fmt.Println("err api get json:", errJson, string(bytesResult))
+		return []byte(""), errJson
+	}
+
+	return bytesResult, nil
+}
+
+func ApiPost(userToken string, apiUrl, apiPath string, data interface{}, result interface{}) ([]byte, error) {
+	url := apiUrl + strings.ToLower(apiPath)
+	regSelfUrl := regexp.MustCompile("^https?:")
+	// 强行是用自己的 domain
+	if regSelfUrl.MatchString(apiPath) {
+		url = apiPath
+	}
+
+	req := httplib.Get(url)
+	req.SetTLSClientConfig(&tls.Config{InsecureSkipVerify: true})
+	dataByte, err := json.Marshal(data)
+
+	postData := Encrypt(string(dataByte))
+
+	// 子系统名称
+	appName, _ := beego.AppConfig.String("appName")
+	reg := regexp.MustCompile("^api-")
+	subSystem := reg.ReplaceAllString(appName, "")
+	req.Param("subSystem", subSystem)
+
+	req.Header("Content-Type", "application/json")
+
+	//fmt.Println("ApiGet:", global.IdEncrypt(userId), url, postData)
+
+	req.Body(map[string]string{
+		"data":      postData,
+		"userToken": userToken,
+		"subSystem": subSystem,
+	})
+
+	bytesResult, err := req.Bytes()
+	if err != nil {
+		fmt.Println("post api:", err)
+		return []byte(""), err
+	}
+
+	//query := ThirdIpResult{}
+	//errJson := json.Unmarshal(bytesResult, &query)
+	//if errJson != nil {
+	//	fmt.Println("err 21:", errJson)
+	//	return ""
+	//}
+
+	errJson := json.Unmarshal(bytesResult, result)
+	if errJson != nil {
+		fmt.Println("err post 1:", url)
+		fmt.Println("err api post json:", errJson, string(bytesResult))
 		return []byte(""), errJson
 	}
 
